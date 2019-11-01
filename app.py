14af8e5e3ec83d245de26be8839394f18c8e3f98
allten <skalstjd14@gmail.com>
2019-11-01 13:43:53 +0900

    全体的なコード修正

--- app.py
+++ app.py
@@ -1,30 +1,51 @@
-# server.py
-from flask import Flask, request, render_template
-from gevent import pywsgi
+# -*- coding: utf-8 -*-
+import json
+import datetime
+import time
+
+from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 
+from flask import Flask, request, render_template
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+   return render_template('index.html')
 
 
 @app.route('/pipe')
 def pipe():
-    if request.environ.get('wsgi.websocket'):
-        ws = request.environ['wsgi.websocket']
+   if request.environ.get('wsgi.websocket'):
+       ws = request.environ['wsgi.websocket']
+       while True:
+           time.sleep(1)
+           message = ws.receive()
+           if message is None:
+               break
+           datetime_now = datetime.datetime.now()
+           data = {
+               'time': str(datetime_now),
+               'message': message
+           }
+           ws.send(json.dumps(data))
+           print(message)
+           print(data)
+   return
 
-        while True:
-            ws.send(input())
 
+if __name__ == '__main__':
+   app.debug = True
 
-def main():
-    app.debug = True
-    server = pywsgi.WSGIServer(("", 8080), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+   host = 'localhost'
+   port = 8080
 
-
-if __name__ == "__main__":
-    main()
+   host_port = (host, port)
+   server = WSGIServer(
+       host_port,
+       app,
+       handler_class=WebSocketHandler
+   )
+   server.serve_forever()
