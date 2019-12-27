98ba9b57e13249929293db72776167e4ff611969
allten <skalstjd14@gmail.com>
2019-12-27 17:31:47 +0900

    20191227 17:31

--- app.py
+++ app.py
@@ -1,45 +1,36 @@
-# flaskから ページのレンダリングモジュールのrender_templateをロード
-# flask-socketioから SocketIO, room関係のモジュールをロード
-
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
-from wtforms import StringField, SubmitField
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
-
-# Flaskの初期化、シークレットキーの指定を行う。
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1@3$5s^7S"
 socketio = SocketIO(app)
 
-# メインページ(index)ページでは、GET,POSTメソードを使用し、render_templateモジュールでmain.htmlをロードする。
-
 class LoginForm(FlaskForm):
-    # name = StringField('Name', validators=[InputRequired()])
     room = StringField('Room', validators=[InputRequired()])
     submit = SubmitField('Enter Chatroom')
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    form = LoginForm()
+    form = LoginForm(request.form)
     if form.validate_on_submit():
-        # session['name'] = form.name.data
         session['room'] = form.room.data
         return redirect(url_for("chat"))
     elif request.method == 'GET':
-        # form.name.data = session.get('name', '')
         form.room.data = session.get('room', '')
-    return render_template('main.html', form=form, errors=form.errors.items())
+    return render_template('main.html', form=form)
 
-
-@app.route("/chat", methods=['GET', 'POST'])
+@app.route("/<room>", methods=['GET', 'POST'])
 def chat():
-    # name = session.get('name', '')
-    room = session.get("room", '')
+    room = session.get('room', '')
     if room == '':
-        return redirect(url_for("index"))
+        return redirect(url_for('index'))
     return render_template('chat.html', room=room)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
 @socketio.on("message")
 def handle_messages(json, methods=['GET', 'POST']):
